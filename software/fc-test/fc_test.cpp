841b691b9fbd50d4a71fc474eac7498df07bd7a0
hubuyu <tuanhe@126.com>
2020-03-14 20:12:57 +0800

    update the .S code

--- software/fc-test/fc_test.cpp
+++ software/fc-test/fc_test.cpp
@@ -7,11 +7,35 @@
 
 void *__dso_handle;
 
-using namespace std; 
+using namespace std;
+
+int bin_test_asm(void) ;
+
+static void test_asm(void)
+{
+  int sum ;
+  int add1 = 100 ;
+  int add2 = 200 ; 
+  printf("enetring %s\n", __FUNCTION__);
+  __asm__ __volatile__ (
+    "add %[dest] , %[src1] , %[src2]"
+    :[dest]"=r"(sum)
+    :[src1]"r"(add1) ,[src2]"r"(add2)
+  );
+  
+  printf("sum = %d\n", sum);
+  printf("leaving %s\n", __FUNCTION__);
+}
 
 int main() { 
   printf("Before call tensorflow-lite micro lib\n");
-  fc_test();
   printf("Before call tensorflow-lite micro lib\n");
- return 0; 
+  //fc_test();
+  printf("Before call tensorflow-lite micro lib\n");
+  test_asm();
+
+  int a = bin_test_asm();
+  printf("a = 0x%x \n", a);
+ 
+  return 0; 
 }
--- software/hello/hello.c
+++ software/hello/hello.c
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+int bin_test_asm(void) ;
+int sub_asm(int a , int b );
+
 int main() {
     printf("Hello, World!\n");
     printf("Hello, World!\n");
@@ -10,7 +13,14 @@ int main() {
     printf("Hello, World!\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
-    #if 1
+    
+    int a = bin_test_asm();
+    printf("a = 0x%x \n", a);
+
+    int b = sub_asm(123, 66);
+    printf("sub = %d \n", b);
+
+    #if 0
     metal_tty_putc("l");
     metal_tty_putc("o");
     metal_tty_putc("v");
